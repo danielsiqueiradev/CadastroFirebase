a53a39fcf6bc1e1b275a07be9b8f0e95ad58ee59
Daniel Siqueira <75674258+danielsiqueiradev@users.noreply.github.com>
2023-11-24 17:47:27 -0300

    programa funcional

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+// import { Container } from './styles';
+
+import Routes from './src/routes';
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#3d9be9" />
+      <Routes />
+    </>
+    
+  );
+};
+
+export default App;
